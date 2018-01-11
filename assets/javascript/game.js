a57911a30c69ff0f1699f910ee559d65d24895d2
Sean Gerbich <sdgerbich@gmail.com>
2018-01-10 21:54:49 -0500

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+// var yourNum = 0;
+// var guessNum = (Math.floor(Math.random() * 81) + 19);
+// var wins = 0;
+// var losses = 0;
+// var cOne = Math.floor(Math.random() * 12) + 1;
+// var cTwo = Math.floor(Math.random() * 12) + 1;
+// var cThree = Math.floor(Math.random() * 12) + 1;
+// var cFour = Math.floor(Math.random() * 12) + 1;
+
+// function lose() {
+//     yourNum = 0;
+//     losses++;
+//     guessNum = (Math.floor(Math.random() * 81) + 19);
+//     $("#special-numberbox").html(guessNum);
+
+//     cOne = Math.floor(Math.random() * 12) + 1;
+//     cTwo = Math.floor(Math.random() * 12) + 1;
+//     cThree = Math.floor(Math.random() * 12) + 1;
+//     cFour = Math.floor(Math.random() * 12) + 1;
+//     $("#loses").html(losses);
+// }
+
+// function win() {
+//     yourNum = 0;
+//     wins++;
+//     guessNum = (Math.floor(Math.random() * 81) + 19);
+//     $("#special-numberbox").html(guessNum);
+
+//     cOne = Math.floor(Math.random() * 12) + 1;
+//     cTwo = Math.floor(Math.random() * 12) + 1;
+//     cThree = Math.floor(Math.random() * 12) + 1;
+//     cFour = Math.floor(Math.random() * 12) + 1;
+//     $("#wins").html(wins);
+// }
+
+// if (guessNum === yourNum) {
+
+
+//     wins();
+// }
+
+// if (yourNum > guessNum) {
+
+
+//     lose();
+// }
+// // function setUp() {
+// //     $(document).ready(function () {
+// //         $('#cOne').click(function () {
+// //             yourNum = yourNum + cOne;
+
+// //         })
+// //     })
+// // }
+// // setUp();
